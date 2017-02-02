4dc9609a37c9c79762b6fd738b3495fe2a8f2b93
Geert <sjoerder@yahoo.com>
2017-02-02 14:10:55 +0100

    Bonjour les cons

--- GitCursus/Program.cs
+++ GitCursus/Program.cs
@@ -10,7 +10,7 @@ namespace GitCursus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Suckers!");
+            Console.WriteLine("Bonjour les connards et connasses!");
         }
     }
 }
